55c5e8143d2c87a23c244e685715e297e9d5fe5a
unknown <traviswsnapp@outlook.com>
2023-07-19 21:46:07 -0400

    made Routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const Login = () => {
+  return <h2>Login</h2>;
+};
+
+const FriendsList = () => {
+  return <h2>Friend List</h2>;
+};
+
+const AddFriend = () => {
+  return <h2>Add Friend</h2>;
+};
 
 function App() {
   return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/friends" element={<FriendsList />} />
+          <Route exact path="/friends/add" element={<AddFriend />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import { createRoot } from 'react-dom/client'
-import App from './App'
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById("root"));
 
-root.render(
-  <Router>
-    <App />
-  </Router>
-)
+root.render(<App />);
